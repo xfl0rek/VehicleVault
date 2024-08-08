0c84a0f2934dae9e77bb124a45a0ffce31870e56
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-08-08 19:56:16 +0200

    Added SpringWebInitializer and WebConfig classes.

---
+++ backend/src/main/java/pl/VehicleVault/config/WebConfig.java
@@ -0,0 +1,9 @@
+package pl.VehicleVault.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pl.VehicleVault")
+public class WebConfig {
+}
