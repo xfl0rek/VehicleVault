e6345ca70b9477ab353ea90ef90dc00e1b6cd1c4
Wojciech Florczak <florczak.wojciechv@gmail.com>
2024-08-08 21:23:54 +0200

    Added simple service and controller ; docker container setup.

---
+++ backend/src/main/java/pl/VehicleVault/controller/HelloController.java
@@ -0,0 +1,23 @@
+package pl.VehicleVault.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.VehicleVault.service.HelloService;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    private final HelloService helloService;
+
+    @Autowired
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping
+    public String getHello() {
+        return helloService.getHello();
+    }
+}
---
+++ backend/src/main/java/pl/VehicleVault/service/HelloService.java
@@ -0,0 +1,10 @@
+package pl.VehicleVault.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    public String getHello() {
+        return "Hello World";
+    }
+}
